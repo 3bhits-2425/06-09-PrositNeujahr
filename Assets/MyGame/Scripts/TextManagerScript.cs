dde5054872ac164079981012ca423ba7f07b9225
Georg Fischinger <georg.fischinger07@htl-salzburg.ac.at>
2025-01-08 09:28:04 +0100

    fixed typo

--- Assets/MyGame/Scripts/TextManagerScript.cs
+++ Assets/MyGame/Scripts/TextManagerScript.cs
@@ -5,7 +5,7 @@ public class TextManagerScript : MonoBehaviour
 {
     [SerializeField] private GameObject textPrefab;
     [SerializeField] private float posIncrementSize;
-    [SerializeField] private int endPosXCoordiante;
+    [SerializeField] private int endPosYCoordiante;
     [SerializeField] private float textScale;
     private float scaleFactor = 0.01f;
     Vector3 startPos;
@@ -18,7 +18,7 @@ public class TextManagerScript : MonoBehaviour
     {
         float finalTextScale = textScale * scaleFactor;
         startPos = this.transform.Find("TextStartPos").position;
-        endPos = startPos + new Vector3(0, endPosXCoordiante, 0);
+        endPos = startPos + new Vector3(0, endPosYCoordiante, 0);
 
         textObject = Instantiate(textPrefab, startPos, Quaternion.identity, transform);
         textObject.transform.localScale = new Vector3(finalTextScale, finalTextScale, finalTextScale);
@@ -29,7 +29,7 @@ public class TextManagerScript : MonoBehaviour
         Vector3 currentPos = textObject.transform.position;
         if (currentPos.y < endPos.y)
         {
-            currentPos.y += new Vector3(0, posIncrementSize, 0).y * Time.deltaTime; // deltaTime damit nicht Frame dependent ist
+            currentPos.y += new Vector3(0, posIncrementSize, 0).y * Time.deltaTime; // deltaTime damit nicht Frame dependent ist 
             textObject.transform.position = currentPos;
         }
     }
